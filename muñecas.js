e6cc856c1978d5a430279b5afbcbeaaa101ebef5
ale cervantes <alemaravilla.05@gmail.com>
2023-05-31 20:08:20 -0700

    agrege propuesta propia como ejercicio de muñecas 1

--- muñecas.js
+++ muñecas.js
@@ -1,12 +1,28 @@
 pesoPayaso = 112;
 pesoMuñeca = 75;
 
-payasoVendido =27;
+/*payasoVendido =27;
 muñecaVendida =14;
 
 pesoVendidoPayaso = pesoPayaso * payasoVendido;
 pesoVendidoMuñeca = pesoMuñeca * muñecaVendida;
 totalDelPedido= pesoVendidoPayaso + pesoVendidoMuñeca /1000;
 
-console.log("Peso total del Paquete:" + totalDelPedido+ "kg");
+console.log("Peso total del Paquete:" + totalDelPedido+ "kg"); */
 
+//mi propuesta
+pesoPayaso = 112;
+pesoMuñeca = 75;
+
+payasoVendido =27;
+muñecaVendida =14;
+
+function ventaEnGramos(){
+    pesoTotalPayasos= (pesoPayaso * payasoVendido) ;
+    pesoTotalMuñecas= (pesoMuñeca * muñecaVendida) ;
+    operacion = ((pesoTotalPayasos + pesoTotalMuñecas) /1000) ; 
+    Total= pesoTotalPayasos+pesoTotalMuñecas
+    
+    console.log(Total); 
+}
+ventaEnGramos();
