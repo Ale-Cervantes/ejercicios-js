b417f95dc8afbb6ebdc40f5c612122c5af7f7348
ale cervantes <alemaravilla.05@gmail.com>
2023-06-04 21:39:54 -0700

    se agregaron ejercicios de ciclos, swich y metodos

---
+++ horoscopos.js
@@ -0,0 +1,65 @@
+var signosDelZodiaco = prompt("Ingresa tu signo del zodiaco mi ciela");
+
+
+switch (signosDelZodiaco) {
+    case "aries":
+        console.log(
+            "SIRIUS BLACk");
+        break;
+
+    case "acuario":
+
+        console.log("ALBUS DUMBLEDORE.");
+        break;
+    case  "Capricornio":
+        console.log("SEVRUS SNAPE");
+        break;
+
+    case "cancer":
+        console.log("DOBY");
+        break;
+    case "geminis":
+        console.log(
+            "FRED Y GEORGE WEASLY");
+        break;
+
+    case  "psicis":
+        console.log("Neville Longbottom"); 
+         
+            break;
+
+    case  "leo":
+        console.log(" HARRY POTTER");
+         
+            break;
+
+    case "tauro":
+         console.log("RON WEASLY  Disfrutas de las comodidades de la creación, pero también eres muy consciente de las necesidades de los otros. Puede que a veces te sientas como un secundario, pero al final eres el héroe de tu propia historia.");
+         
+            break;
+
+    case "virgo":
+        console.log(" HERMIONE GREANGER");
+
+        break;
+
+    case "Sagitario":
+      console.log("HAGRID");   
+    
+            break;
+
+    case "libra":
+    console.log("LUNA LOVEGOOD");    
+    
+            break;
+
+    case "escorpio":
+      console.log("VOLDEMORT");  
+        
+
+
+
+            default:
+        console.log("ese no es un signo zodiacal, tu y yo lo sabemos mi ciela");
+        break;
+}
